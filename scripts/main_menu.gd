21f5b55b921011d63c333de2680b0a9893c5a8df
countingbeeps <gamedungeon@protonmail.com>
2025-01-02 12:00:02 -0600

    Start Setup Menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_play_pressed() -> void:
 	#TODO: Go to game scene
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://setup_menu.tscn")
 
 
 func _on_settings_pressed() -> void:
