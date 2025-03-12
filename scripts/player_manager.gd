57c110d84228d6655d1714f38aa20f45f72896dc
RealDarkStudios <94769968+RealDarkStudios@users.noreply.github.com>
2025-03-11 23:05:28 -0500

    switch to spaces instead of tabs

--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -6,20 +6,20 @@ var player_scene: PackedScene = preload("res://player.tscn")
 var players: Array[Node2D]
 
 func _ready() -> void:
-	for player in GlobalData.player_data:
-		var player_node = player_scene.instantiate()
-		player_node.set_player_data(player)
+    for player in GlobalData.player_data:
+        var player_node = player_scene.instantiate()
+        player_node.set_player_data(player)
 
-		add_child(player_node)
-		players.append(player_node)
+        add_child(player_node)
+        players.append(player_node)
 
 func _process(delta: float) -> void:
-	pass
-	
+    pass
+    
 func set_state(player_state):
-	for player in players:
-		player.FrogCurrentState = player_state
+    for player in players:
+        player.FrogCurrentState = player_state
 
 func trigger_throw():
-	for player in players:
-		player.throw_frog()
+    for player in players:
+        player.throw_frog()
