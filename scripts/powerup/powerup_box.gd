13fd51e2db9c9e4a2796cb73b8c79753be358362
RealDarkStudios <94769968+RealDarkStudios@users.noreply.github.com>
2025-03-14 04:00:44 -0500

    change powerup box, start level 3, finish level 2

--- scripts/powerup/powerup_box.gd
+++ scripts/powerup/powerup_box.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
+@export var icon: Sprite2D
+@export var rotation_speed: float = 2
+@export var rotation_max_angle: float = 5.0
+
+var rotation_offset;
 var powerup: Powerup
 
 func _ready() -> void:
+    rotation_offset = randi_range(-20, 20)
     powerup = get_parent().random()
+
+func _process(delta: float) -> void:
+    var angle = sin(Time.get_ticks_msec() / 1000 * rotation_speed + rotation_offset) * rotation_max_angle
+    icon.rotation_degrees = angle
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
     var player := body as PlayerClass
