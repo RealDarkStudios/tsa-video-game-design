a7dca7e0b1ee96c357907e98f0f8c9680d8deb48
RealDarkStudios <94769968+RealDarkStudios@users.noreply.github.com>
2025-03-13 21:28:30 -0500

    add finish menu podium

--- scripts/finish_menu.gd
+++ scripts/finish_menu.gd
@@ -4,16 +4,45 @@ class_name FinishMenu
 @export var game_manager: GameManager
 @export var level_manager: LevelManager
 
+@export var first_tex: TextureRect
+@export var second_tex: TextureRect
+@export var third_tex: TextureRect
+@export var first_name: RichTextLabel
+@export var second_name: RichTextLabel
+@export var third_name: RichTextLabel
+
 func _ready() -> void:
-	pass
+    pass
+    
+func show_rankings() -> void:
+    var num = 2
+    var texs: Array[TextureRect] = [first_tex, second_tex]
+    var names: Array[RichTextLabel] = [first_name, second_name]
+    
+    if len(GlobalData.player_data) < 3:
+        third_name.get_parent().get_parent().queue_free()
+    else:
+        num = 3
+        texs.append(third_tex)
+        names.append(third_name)
+        
+    for i in range(num):
+        var player: PlayerData = level_manager.level_instance.rankings[i]
+        var tex: TextureRect = texs[i]
+        var name_label: RichTextLabel = names[i]
+        
+        tex.texture = player.player_type.texture
+        name_label.text = player.player_name
+        
+    visible = true
 
 func _on_next_level_pressed() -> void:
-	for player in GlobalData.player_data:
-		print("%s, %s, %s" % [player.player_id, player.player_name, player.score])
+    for player in GlobalData.player_data:
+        print("%s, %s, %s" % [player.player_id, player.player_name, player.score])
 
-	level_manager.next_level()
-	game_manager.reset()
-	self.visible = false
+    level_manager.next_level()
+    game_manager.reset()
+    self.visible = false
 
 func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+    get_tree().change_scene_to_file("res://main_menu.tscn")
--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -4,3 +4,5 @@ class_name LevelBase
 @export var finish_line: Area2D
 @export var powerup_manager: PowerupManager
 @export var scores: Array[int]
+
+var rankings: Array[PlayerData]
